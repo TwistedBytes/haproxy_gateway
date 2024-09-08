adc9f521b8d16e370c6d31c4763c57b0058bd0ee
Derk Gortemaker <derk@gortemaker.nl>
2024-09-08 12:58:32 +0200

    fixes / logging explicit in systemd systemd usetmp cache views in package

--- rpm/after-install.sh
+++ rpm/after-install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# clean up old dirs
+[[ -d /var/log/haproxy-gateway/framework ]] && rm -Rf /var/log/haproxy-gateway/framework
+[[ -d /var/log/haproxy-gateway/logs ]] && rm -Rf /var/log/haproxy-gateway/logs/
+#
+
 systemctl daemon-reload
 systemctl is-active --quiet haproxy-gateway && systemctl restart haproxy-gateway
